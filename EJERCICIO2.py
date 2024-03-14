aeeb54a3c6868c6ee15db2bd87eb8f456b4097b5
nassry99 <162034473+nassry99@users.noreply.github.com>
2024-03-14 23:04:45 +0000

    SOLUCION 2

---
+++ EJERCICIO2.py
@@ -0,0 +1,26 @@
+#Problema 2:
+#Escriba un programa en Python para construir el siguiente patrón.
+#*
+#* *
+#* * *
+#* * * *
+#* * * * *
+#* * * *
+#* * *
+#* *
+#*
+
+# numero de filas ahsta la mita
+filas = 5
+
+# imprimir la parte de arriba
+for i in range(filas):
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
+
+# imprimir la parte de abajo
+for i in range(filas - 1, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
